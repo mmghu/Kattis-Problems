91e76c180b9361319f1c89289d847b6691816d31
mmghuneim <muaeim@gmail.com>
2015-12-05 16:19:42 -0500

    Hello World Solution
    Java Solution to Kattis Problem Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Purpose: Print Hello World to the user
+// Preconditions: None
+// Postconditions: Outputs "Hello World!"
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}// end main
+}// end class
