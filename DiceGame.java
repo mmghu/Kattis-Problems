ffed4b0ce75af41fd0650d4dd535dae6e186807f
mmghuneim <muaeim@gmail.com>
2015-12-10 11:43:31 -0500

    Dice Game Solution
    Java Solution to Kattis Problem Dice Game

--- DiceGame.java
+++ DiceGame.java
@@ -4,21 +4,15 @@ import java.io.InputStreamReader;
 
 public class DiceGame {
 	
-	public static double avgRoll(String[] player){
+	public static double sum(String[] player){
 		// Convert string array into variables
 		int a = Integer.parseInt(player[0]);
 		int b = Integer.parseInt(player[1]);
 		int c = Integer.parseInt(player[2]);
 		int d = Integer.parseInt(player[3]);
 		
-		// Sum of the two ranges
-		int acc1 = 0; int acc2 = 0;
-		for(int i = a; i <= b; i++){acc1 += i;}
-		for(int i = c; i <= d; i++){acc2 += i;}
-		
-		// Return average roll
-		return acc1/(b-a+1) + acc2/(d-c+1);
-	} // end avgRoll
+		return a + b + c + d;
+	} // end sum
 	
 	public static void main(String[] args) throws IOException {
 		// Get both dice rolls
@@ -27,10 +21,10 @@ public class DiceGame {
 		String[] Emma = reader.readLine().split(" "); 
 		
 		// Calculate average rolls
-		if(avgRoll(Gunnar) > avgRoll(Emma)){
+		if(sum(Gunnar) > sum(Emma)){
 			System.out.println("Gunnar");
 		}
-		else if(avgRoll(Gunnar) < avgRoll(Emma)){
+		else if(sum(Gunnar) < sum(Emma)){
 			System.out.println("Emma");
 		}
 		else{
