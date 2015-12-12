96030e77c9a36a6c93fe5569ff4f57c096482924
mmghuneim <muaeim@gmail.com>
2015-12-11 21:55:44 -0500

    Apaxians Solution
    Java Solution to Kattis Problem Apaxians

---
+++ Apaxians.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Apaxians {
+	public static void main(String[] args) throws IOException {
+		// Get input
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String word = reader.readLine();
+		
+		// Remove duplicate letters (regex)
+		System.out.println(word.replaceAll("(.)\\1{1,}", "$1"));
+		
+	} // end main
+} // end class
